daff3e95ce1bb19ca8aa99627d635341e6099a10
Ivan Chernykh <ivanchernykh98@gmail.com>
2024-09-04 16:17:34 +0300

    added monster prefab, bathroom prop, refactoring

--- Assets/Scripts/WorldItems/WorldItem.cs
+++ Assets/Scripts/WorldItems/WorldItem.cs
@@ -2,11 +2,16 @@ using UnityEngine;
 
 public class WorldItem : Interactable {
     [SerializeField] private ItemData itemData;
+    [SerializeField] private string pickUpMessage;
     private void Start() {
         interactMessage = "Take";
+        if (pickUpMessage.Length == 0) {
+            pickUpMessage = $"Picked up {itemData.itemName}";
+        }
     }
     public override void Interact() {
         PlayerInventory.Instance.AddItem(itemData);
+        TooltipUI.Instance.Show(pickUpMessage);
         Destroy(gameObject);
     }
 }
