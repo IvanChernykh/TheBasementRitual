ad96f99b10ddbbca21f20586242f25acdc3a29e0
Ivan Chernykh <ivanchernykh98@gmail.com>
2024-09-05 20:12:27 +0300

    first game event - closing door

---
+++ Assets/Scripts/ClosingDoorEvent.cs
@@ -0,0 +1,44 @@
+using System;
+using UnityEngine;
+
+public class ClosingDoorEvent : MonoBehaviour {
+    private bool eventIsTriggered;
+    [SerializeField] private Transform door;
+    [SerializeField] private Transform rotationPoint;
+    [SerializeField] private GameObject roomLight;
+    [SerializeField] private float openSpeed = 140f;
+
+    private bool isClosed;
+    private float maxOpenAngle = 30f;
+    private float currentAngle;
+
+
+    private void Start() {
+        door.RotateAround(rotationPoint.position, door.up, maxOpenAngle);
+        currentAngle = maxOpenAngle;
+    }
+    private void Update() {
+        if (eventIsTriggered && !isClosed) {
+            HandleClose();
+        }
+    }
+    private void OnTriggerEnter(Collider other) {
+        if (!eventIsTriggered) {
+            if (other.CompareTag("Player")) {
+                eventIsTriggered = true;
+            }
+        }
+    }
+
+    private void HandleClose() {
+        float angleToRotate = openSpeed * Time.deltaTime;
+        float remainingAngle = currentAngle;
+        if (angleToRotate >= remainingAngle) {
+            angleToRotate = remainingAngle;
+            isClosed = true;
+            roomLight.SetActive(false);
+        }
+        door.RotateAround(rotationPoint.position, door.up, -angleToRotate);
+        currentAngle -= angleToRotate;
+    }
+}
