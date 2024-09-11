74068c613221d3d4c51b42559fe705e4264e8913
Ivan Chernykh <ivanchernykh98@gmail.com>
2024-09-11 22:39:47 +0300

    trigger event refactor

--- Assets/Scripts/Events/OnTriggerEnter/ActivateOrDeactivateObject.cs
+++ Assets/Scripts/Events/OnTriggerEnter/ActivateOrDeactivateObject.cs
@@ -1,15 +1,34 @@
+using System.Collections;
 using UnityEngine;
 
 public class ActivateOrDeactivateObject : OnTriggerEnterBase {
     [SerializeField] private GameObject objectToShow;
     [SerializeField] private GameObject objectToHide;
+    [SerializeField] private float showDelay = 0f;
+    [SerializeField] private float hideDelay = 0f;
 
     protected override void HandleEvent() {
         if (objectToShow != null) {
-            objectToShow.SetActive(true);
+            if (showDelay > 0) {
+                StartCoroutine(ShowWithDelay());
+            } else {
+                objectToShow.SetActive(true);
+            }
         }
         if (objectToHide != null) {
-            objectToHide.SetActive(false);
+            if (hideDelay > 0) {
+                StartCoroutine(HideWithDelay());
+            } else {
+                objectToHide.SetActive(false);
+            }
         }
+    }
+    private IEnumerator ShowWithDelay() {
+        yield return new WaitForSeconds(showDelay);
+        objectToShow.SetActive(true);
+    }
+    private IEnumerator HideWithDelay() {
+        yield return new WaitForSeconds(hideDelay);
+        objectToHide.SetActive(false);
     }
 }
--- Assets/Scripts/Events/OnTriggerEnter/PlaySound.cs
+++ Assets/Scripts/Events/OnTriggerEnter/PlaySound.cs
@@ -1,13 +1,27 @@
 using UnityEngine;
 
 public class PlaySound : OnTriggerEnterBase {
-    [SerializeField] private AudioClip[] audioClips;
-    [SerializeField] private Vector3 positionToPlay;
+    [Header("AudioClip")]
+    [SerializeField] private AudioClip audioClip;
+    [SerializeField] private Transform positionToPlay;
     [SerializeField] private float volume = 1f;
     [SerializeField] private float minDistance = 1f;
     [SerializeField] private float maxDistance = 10f;
 
+
+    [Header("AudioSource")]
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private float fadeTime = 0f;
+
     protected override void HandleEvent() {
-        SoundManager.Instance.PlaySound(audioClips, positionToPlay, volume, minDistance, maxDistance);
+        if (audioClip != null) {
+            SoundManager.Instance.PlaySound(audioClip, positionToPlay.position, volume, minDistance, maxDistance);
+        } else if (audioSource != null) {
+            if (fadeTime == 0f) {
+                SoundManager.Instance.PlayAudioSource(audioSource);
+            } else {
+                SoundManager.Instance.FadeInAudioSource(audioSource, fadeTime);
+            }
+        }
     }
 }
