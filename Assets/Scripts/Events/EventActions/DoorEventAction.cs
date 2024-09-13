5973eb0a1fa0a46d76e4136319191464c780361f
Ivan Chernykh <ivanchernykh98@gmail.com>
2024-09-13 12:39:59 +0300

    inhanced door event action

--- Assets/Scripts/Events/EventActions/DoorEventAction.cs
+++ Assets/Scripts/Events/EventActions/DoorEventAction.cs
@@ -1,13 +1,27 @@
+using System.Collections;
 using UnityEngine;
 
 public class DoorEventAction : EventAction {
     [SerializeField] private Door door;
     [Tooltip("Open or close"), SerializeField] private bool openDoor;
+    [SerializeField] private float delay;
+
     public override void ExecuteEvent() {
+        if (delay > 0) {
+            StartCoroutine(OpenCloseWithDelay());
+        } else {
+            OpenCloseDoor();
+        }
+    }
+    private void OpenCloseDoor() {
         if (openDoor) {
             door.OpenDoor();
         } else {
             door.CloseDoor();
         }
+    }
+    private IEnumerator OpenCloseWithDelay() {
+        yield return new WaitForSeconds(delay);
+        OpenCloseDoor();
     }
 }
