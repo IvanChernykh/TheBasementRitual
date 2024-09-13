98e26a155e81d0d20c5a6b0c7fa7033b7275c949
Ivan Chernykh <ivanchernykh98@gmail.com>
2024-09-13 16:28:09 +0300

    door audio refactor

--- Assets/Scripts/DoorAudio.cs
+++ Assets/Scripts/DoorAudio.cs
@@ -2,15 +2,16 @@ using UnityEngine;
 
 public class DoorAudio : MonoBehaviour {
     public static DoorAudio Instance { get; private set; }
+    [Header("Sounds")]
     [SerializeField] private AudioClip[] locked;
     [SerializeField] private AudioClip[] close;
     [SerializeField] private AudioClip[] open;
     [SerializeField] private AudioClip[] closeDoubleDoor;
-    private float minSoundDistance = .5f;
-    private float maxSoundDistance = 5f;
-    private float minOpenSoundDistance = .6f;
+    [Header("Options")]
+    [SerializeField] private float minSoundDistance = .8f;
+    [SerializeField] private float maxSoundDistance = 6f;
+    [SerializeField] private float closeVolume = .5f;
     private float defaultVolume = 1f;
-    private float closeVolume = .5f;
 
     private void Awake() {
         if (Instance == null) {
@@ -25,7 +26,7 @@ public class DoorAudio : MonoBehaviour {
         SoundManager.Instance.PlaySound(close, position, closeVolume, minSoundDistance, maxSoundDistance);
     }
     public void PlayOpen(Vector3 position) {
-        SoundManager.Instance.PlaySound(open, position, defaultVolume, minOpenSoundDistance, maxSoundDistance);
+        SoundManager.Instance.PlaySound(open, position, defaultVolume, minSoundDistance, maxSoundDistance);
     }
     public void PlayCloseDoubleDoor(Vector3 position) {
         SoundManager.Instance.PlaySound(closeDoubleDoor, position, closeVolume, minSoundDistance, maxSoundDistance);
