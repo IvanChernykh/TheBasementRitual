0d8993256a88cde2ef29f29e3257d67fc384f83e
Ivan Chernykh <ivanchernykh98@gmail.com>
2024-09-15 21:19:10 +0300

    basic monster patrol state

--- Assets/Scripts/Monster/MonsterController.cs
+++ Assets/Scripts/Monster/MonsterController.cs
@@ -1,7 +1,41 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class MonsterController : MonoBehaviour {
+    public enum State {
+        Patrolling,
+        ChasingPlayer,
+        SearchingPlayer,
+    }
+    private State currentState = State.Patrolling;
 
+    [Header("Settings")]
+    [SerializeField] private NavMeshAgent agent;
+    [SerializeField] private MonsterAnimation monsterAnimation;
+
+    [Header("Patrol State")]
+    [SerializeField] private Transform[] patrolPoints;
+    private int nextPointIdx = 0;
+    private bool one;
+
+    private void Update() {
+        switch (currentState) {
+            case State.Patrolling:
+                HandlePatrol();
+                break;
+            case State.ChasingPlayer:
+                break;
+            case State.SearchingPlayer:
+                break;
+        }
+    }
+    private void HandlePatrol() {
+        if (!one) {
+            monsterAnimation.Run();
+        }
+        if (agent.remainingDistance < 0.2f) {
+            nextPointIdx = Random.Range(0, patrolPoints.Length);
+        }
+        agent.SetDestination(patrolPoints[nextPointIdx].position);
+    }
 }
