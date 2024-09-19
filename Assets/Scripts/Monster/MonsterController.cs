2f8be80bfc8fce472b6ccd1d1bf4c8943222e68b
Ivan Chernykh <ivanchernykh98@gmail.com>
2024-09-19 17:27:00 +0300

    minor fix

--- Assets/Scripts/Monster/MonsterController.cs
+++ Assets/Scripts/Monster/MonsterController.cs
@@ -1,9 +1,7 @@
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using Assets.Scripts.Utils;
 using UnityEngine;
-using UnityEngine.AI;
 
 public class MonsterController : MonoBehaviour {
     public enum State {
