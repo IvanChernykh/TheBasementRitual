604db4a52a9f1ecc1d19011308a32a239a554489
Ivan Chernykh <ivanchernykh98@gmail.com>
2024-09-20 23:14:27 +0300

    sound manager refactoring

--- Assets/Scripts/Managers/SoundManager.cs
+++ Assets/Scripts/Managers/SoundManager.cs
@@ -4,6 +4,16 @@ using Assets.Scripts.Utils;
 
 public class SoundManager : MonoBehaviour {
     public static SoundManager Instance { get; private set; }
+    private class PlayClipSettings {
+        public AudioClip Clip { get; set; }
+        public Vector3 Position { get; set; }
+        public bool Is3D { get; set; } = true;
+        public float Volume { get; set; } = 1f;
+        public float MinDistance { get; set; } = 1f;
+        public float MaxDistance { get; set; } = 5f;
+        public float Pitch { get; set; } = 1f;
+        public AudioRolloffMode RolloffMode { get; set; } = AudioRolloffMode.Logarithmic;
+    };
 
     private void Awake() {
         if (Instance != null) {
@@ -21,7 +31,16 @@ public class SoundManager : MonoBehaviour {
         float pitch = 1f,
         AudioRolloffMode rolloffMode = AudioRolloffMode.Logarithmic
         ) {
-        PlayClip3D(audioclip, position, volume, minDistance, maxDistance, pitch, rolloffMode);
+        PlayClip(new PlayClipSettings {
+            Clip = audioclip,
+            Position = position,
+            Is3D = true,
+            Volume = volume,
+            MinDistance = minDistance,
+            MaxDistance = maxDistance,
+            Pitch = pitch,
+            RolloffMode = rolloffMode
+        });
     }
     public void PlaySound(
         AudioClip[] audioClipArr,
@@ -32,14 +51,33 @@ public class SoundManager : MonoBehaviour {
         float pitch = 1f,
         AudioRolloffMode rolloffMode = AudioRolloffMode.Logarithmic
         ) {
-        PlaySound(audioClipArr[Random.Range(0, audioClipArr.Length)], position, volume, minDistance, maxDistance, pitch, rolloffMode);
+        PlayClip(new PlayClipSettings {
+            Clip = audioClipArr[Random.Range(0, audioClipArr.Length)],
+            Position = position,
+            Is3D = true,
+            Volume = volume,
+            MinDistance = minDistance,
+            MaxDistance = maxDistance,
+            Pitch = pitch,
+            RolloffMode = rolloffMode
+        });
     }
 
     public void PlaySound2D(AudioClip audioClip, Vector3 position, float volume = 1f) {
-        PlayClip2D(audioClip, position, volume);
+        PlayClip(new PlayClipSettings {
+            Clip = audioClip,
+            Position = position,
+            Volume = volume,
+            Is3D = false
+        });
     }
     public void PlaySound2D(AudioClip[] audioClipArr, Vector3 position, float volume = 1f) {
-        PlayClip2D(audioClipArr[Random.Range(0, audioClipArr.Length)], position, volume);
+        PlayClip(new PlayClipSettings {
+            Clip = audioClipArr[Random.Range(0, audioClipArr.Length)],
+            Position = position,
+            Volume = volume,
+            Is3D = false
+        });
     }
     // AudioSource
     public void PlayAudioSource(AudioSource audioSource) {
@@ -60,91 +98,71 @@ public class SoundManager : MonoBehaviour {
             audio.Pause();
         }
     }
+    // fading audio source
+    public Coroutine FadeInAudioSource(AudioSource audio, float fadeTime) {
+        float initialVolume = audio.volume;
+        audio.volume = 0;
+        audio.Play();
+        return StartCoroutine(FadeAudio(audio, initialVolume, fadeTime));
+    }
     public void FadeOutAudioSource(AudioSource audio, float fadeTime) {
         if (audio.isPlaying) {
-            StartCoroutine(FadeOut(audio, fadeTime));
+            StartCoroutine(FadeAudio(audio, 0, fadeTime, stopAfterFade: true));
         }
     }
     public void FadeOutPauseAudioSource(AudioSource audio, float fadeTime) {
         if (audio.isPlaying) {
-            StartCoroutine(FadeOutPause(audio, fadeTime));
+            StartCoroutine(FadeAudio(audio, 0, fadeTime, pauseAfterFade: true));
         }
-    }
-    public void FadeInAudioSource(AudioSource audio, float fadeTime) {
-        float initialVolume = audio.volume;
-        audio.volume = 0;
-        audio.Play();
-        StartCoroutine(FadeIn(audio, initialVolume, fadeTime));
     }
     public void SwitchAudio(AudioSource audio1, AudioSource audio2, float fadeTime) {
         FadeOutAudioSource(audio1, fadeTime);
         FadeInAudioSource(audio2, fadeTime);
     }
+    // volume
     public void IncreaseVolume(AudioSource audioSource, float targetVolume, float fadeTime) {
-        StartCoroutine(FadeIn(audioSource, targetVolume, fadeTime));
+        StartCoroutine(FadeAudio(audioSource, targetVolume, fadeTime));
     }
     // private methods
-    private void PlayClip2D(AudioClip clip, Vector3 position, float volume = 1) {
+    private void PlayClip(PlayClipSettings settings) {
         GameObject newObject = new GameObject("One shot audio");
-        newObject.transform.position = position;
+        newObject.transform.position = settings.Position;
         AudioSource audioSource = (AudioSource)newObject.AddComponent(typeof(AudioSource));
-        audioSource.clip = clip;
-        audioSource.spatialBlend = 0f;
-        audioSource.volume = volume;
+
+        audioSource.clip = settings.Clip;
+        audioSource.spatialBlend = settings.Is3D ? 1f : 0f;
+        audioSource.volume = settings.Volume;
+        audioSource.pitch = settings.Pitch;
+        if (settings.Is3D) {
+            audioSource.minDistance = settings.MinDistance;
+            audioSource.maxDistance = settings.MaxDistance;
+            audioSource.rolloffMode = settings.RolloffMode;
+        }
         audioSource.Play();
-        Destroy(newObject, clip.length * ((Time.timeScale < 0.01f) ? 0.01f : Time.timeScale));
+        Destroy(newObject, settings.Clip.length * ((Time.timeScale < 0.01f) ? 0.01f : Time.timeScale));
     }
-    private void PlayClip3D(
-        AudioClip clip,
-        Vector3 position,
-        float volume = 1,
-        float minDistance = 1,
-        float maxDistance = 5f,
-        float pitch = 1f,
-        AudioRolloffMode rolloffMode = AudioRolloffMode.Logarithmic
+
+    // enumerator
+    private IEnumerator FadeAudio(
+        AudioSource audioSource,
+        float targetVolume,
+        float fadeTime,
+        bool stopAfterFade = false,
+        bool pauseAfterFade = false
         ) {
-        GameObject newObject = new GameObject("One shot audio");
-        newObject.transform.position = position;
-        AudioSource audioSource = (AudioSource)newObject.AddComponent(typeof(AudioSource));
+        float delta = targetVolume - audioSource.volume;
 
-        audioSource.clip = clip;
-        audioSource.spatialBlend = 1f;
-        audioSource.volume = volume;
-        audioSource.pitch = pitch;
-        audioSource.minDistance = minDistance;
-        audioSource.maxDistance = maxDistance;
-        audioSource.rolloffMode = rolloffMode;
-
-        audioSource.Play();
-        Destroy(newObject, clip.length * ((Time.timeScale < 0.01f) ? 0.01f : Time.timeScale));
-    }
-
-    // enumerators
-    private IEnumerator FadeOut(AudioSource audioSource, float fadeTime) {
-        float startVolume = audioSource.volume;
-
-        while (audioSource.volume > 0) {
-            audioSource.volume -= startVolume * Time.deltaTime / fadeTime;
+        while (!Mathf.Approximately(audioSource.volume, targetVolume)) {
+            audioSource.volume += delta * Time.deltaTime / fadeTime;
             yield return null;
         }
-        audioSource.Stop();
-        audioSource.volume = startVolume;
-    }
-    private IEnumerator FadeOutPause(AudioSource audioSource, float fadeTime) {
-        float startVolume = audioSource.volume;
 
-        while (audioSource.volume > 0) {
-            audioSource.volume -= startVolume * Time.deltaTime / fadeTime;
-            yield return null;
-        }
-        audioSource.Pause();
-        audioSource.volume = startVolume;
-    }
-    private IEnumerator FadeIn(AudioSource audioSource, float targetVolume, float fadeTime) {
-        while (audioSource.volume < targetVolume) {
-            audioSource.volume += Time.deltaTime / fadeTime * targetVolume;
-            yield return null;
-        }
         audioSource.volume = targetVolume;
+
+        if (stopAfterFade) {
+            audioSource.Stop();
+        } else if (pauseAfterFade) {
+            audioSource.Pause();
+        }
     }
 }
