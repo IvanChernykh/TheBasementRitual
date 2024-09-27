870f8f908446131d7f1a97c6ec9de29304770df9
Ivan Chernykh <ivanchernykh98@gmail.com>
2024-09-27 20:13:07 +0300

    added switchboard

--- Assets/Scripts/Interactables/ActionExecutor.cs
+++ Assets/Scripts/Interactables/ActionExecutor.cs
@@ -2,9 +2,15 @@ using UnityEngine;
 
 public class ActionExecutor : Interactable {
     [SerializeField] private string interactionMessage;
+    [Tooltip("Make non interactable after executing action")]
+    [SerializeField] private bool makeNonInteractable;
 
     private void Start() {
         interactMessage = interactionMessage;
     }
-    protected override void Interact() { }
+    protected override void Interact() {
+        if (makeNonInteractable) {
+            gameObject.layer = LayerMask.NameToLayer("Default");
+        }
+    }
 }
