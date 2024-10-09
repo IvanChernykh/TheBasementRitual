f01effc1623c6d453e32425e48fc0a84e1b0a76c
Ivan Chernykh <ivanchernykh98@gmail.com>
2024-10-09 18:51:44 +0300

    main menu visual

--- Assets/Scripts/UI/MainMenu/MainMenuUI.cs
+++ Assets/Scripts/UI/MainMenu/MainMenuUI.cs
@@ -4,8 +4,12 @@ public class MainMenuUI : MonoBehaviour {
     public void StartNewGameBtn() {
         SceneController.Instance.StartNewGame();
     }
+    // todo: disable if there is no save files
     public void ContinueGameBtn() {
         SceneController.Instance.LoadSavedGame(SaveFileName.DefaultSave);
+    }
+    public void OptionsBtn() {
+
     }
     public void ExitBtn() {
         Application.Quit();
