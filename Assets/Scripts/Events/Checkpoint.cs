3fbaa1ab0259112aa2efbc48b12085fc88a5c1a1
Ivan Chernykh <ivanchernykh98@gmail.com>
2024-10-11 17:04:52 +0300

    checkpoint conditions, minor fixes

--- Assets/Scripts/Events/Checkpoint.cs
+++ Assets/Scripts/Events/Checkpoint.cs
@@ -1,16 +1,31 @@
 using UnityEngine;
 
 public class Checkpoint : MonoBehaviour {
+    [SerializeField] private EventCondition[] conditions;
     [SerializeField] private string id;
     public string Id { get => id; }
     private bool eventIsTriggered;
+
     private void OnTriggerEnter(Collider other) {
         if (!eventIsTriggered) {
             if (other.CompareTag("Player")) {
-                eventIsTriggered = true;
-                SceneStateManager.Instance.AddCheckpoint(id);
-                SaveSystem.SaveGame(showSaveUI: true);
+                bool conditionMet = CheckConditions();
+                if (conditionMet) {
+                    eventIsTriggered = true;
+                    SceneStateManager.Instance.AddCheckpoint(id);
+                    SaveSystem.SaveGame(showSaveUI: true);
+                }
             }
         }
+    }
+    private bool CheckConditions() {
+        bool conditionMet = true;
+        foreach (EventCondition condition in conditions) {
+            if (!condition.IsConditionMet()) {
+                conditionMet = false;
+                break;
+            }
+        }
+        return conditionMet;
     }
 }
