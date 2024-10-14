c8b654ad96a7de1accf06bf926b29b8e36bb9613
Ivan Chernykh <ivanchernykh98@gmail.com>
2024-10-14 19:44:49 +0300

    added condition to interactable events, bug fixes

--- Assets/Scripts/Interactables/Interactable.cs
+++ Assets/Scripts/Interactables/Interactable.cs
@@ -4,16 +4,43 @@ public abstract class Interactable : MonoBehaviour {
     public string interactMessage { get; protected set; }
     [Header("Event Actions")]
     [SerializeField] private EventAction[] interactEventAction;
-    [SerializeField] private bool executeEventActionAlways;
+    [SerializeField] private EventCondition[] interactEventConditions;
+    [Tooltip("Execute event action always"), SerializeField] private bool executeEventActionAlways;
+    [Tooltip("Execute by condition only"), SerializeField] private bool executeByConditionOnly;
     private bool eventExecuted;
     abstract protected void Interact();
     public void InteractAction() {
         Interact();
-        if (!eventExecuted || executeEventActionAlways) {
-            eventExecuted = true;
+
+        bool conditionMet = CheckConditions();
+        if (executeByConditionOnly && conditionMet) {
             foreach (EventAction item in interactEventAction) {
                 item.ExecuteEvent();
             }
+            return;
         }
+        if (!eventExecuted || executeEventActionAlways) {
+            eventExecuted = true;
+
+            if (conditionMet) {
+                foreach (EventAction item in interactEventAction) {
+                    item.ExecuteEvent();
+                }
+            }
+        }
+    }
+
+    private bool CheckConditions() {
+        bool conditionMet = true;
+        if (interactEventConditions == null || interactEventConditions.Length == 0) {
+            return conditionMet;
+        }
+        foreach (EventCondition condition in interactEventConditions) {
+            if (!condition.IsConditionMet()) {
+                conditionMet = false;
+                break;
+            }
+        }
+        return conditionMet;
     }
 }
