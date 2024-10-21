c3500a7ef9921e94d4138c4e9561b004369105bf
Ivan Chernykh <ivanchernykh98@gmail.com>
2024-10-21 10:39:52 +0300

    added remove planks sfx

--- Assets/Scripts/Interactables/WoodenPlank.cs
+++ Assets/Scripts/Interactables/WoodenPlank.cs
@@ -3,12 +3,14 @@ using UnityEngine;
 public class WoodenPlank : Interactable {
     [SerializeField] private ItemData hammer;
     [SerializeField] private BlockedDoor door;
+    [SerializeField] private AudioClip removeSFX;
 
     private void Start() {
         interactMessage = "Remove";
     }
     protected override void Interact() {
         if (PlayerInventory.Instance.HasItem(hammer)) {
+            SoundManager.Instance.PlaySound(removeSFX, transform.position, volume: .5f, pitch: Random.Range(1.2f, 1.8f));
             door.RemovePlank();
             Destroy(gameObject);
         } else {
