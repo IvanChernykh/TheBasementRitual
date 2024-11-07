4808acf356b65e49b806ddd50e8107a86d994841
Ivan Chernykh <ivanchernykh98@gmail.com>
2024-11-07 11:46:36 +0200

    prepared interaction messages

--- Assets/Scripts/UI/InteractionMessageUI.cs
+++ Assets/Scripts/UI/InteractionMessageUI.cs
@@ -1,10 +1,14 @@
 using TMPro;
 using UnityEngine;
 using Assets.Scripts.Utils;
+using UnityEngine.Localization;
 
 public class InteractionMessageUI : MonoBehaviour {
     public static InteractionMessageUI Instance { get; private set; }
+
     [SerializeField] private TextMeshProUGUI message;
+
+    private LocalizedString localizedMessage;
 
     private void Awake() {
         if (Instance != null) {
@@ -17,9 +21,24 @@ public class InteractionMessageUI : MonoBehaviour {
     private void Start() {
         gameObject.SetActive(false);
     }
-    public void Show(string text) {
+
+    private void OnMessageChanged(string localizedText) {
+        message.text = localizedText;
         gameObject.SetActive(true);
-        message.text = text;
+
+        localizedMessage.StringChanged -= OnMessageChanged;
+    }
+
+    public void Show(string textKey) {
+        if (string.IsNullOrEmpty(textKey)) return;
+
+        localizedMessage = new LocalizedString {
+            TableReference = LocalizationTables.Interactions,
+            TableEntryReference = textKey
+        };
+
+        localizedMessage.StringChanged += OnMessageChanged;
+        localizedMessage.RefreshString();
     }
     public void Hide() {
         gameObject.SetActive(false);
