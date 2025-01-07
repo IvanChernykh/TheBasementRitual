e76f5f4a82c953b569d1b9679292c00f85df8c58
Ivan Chernykh <ivanchernykh98@gmail.com>
2025-01-07 14:33:52 +0200

    optaions, added mappings, minor key fixes

--- Assets/Scripts/UI/Panels/OptionsPanel.cs
+++ Assets/Scripts/UI/Panels/OptionsPanel.cs
@@ -3,16 +3,32 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
+public enum TabsEnum {
+    Controls,
+    Sound,
+    Language
+}
+
 public class OptionsPanel : MonoBehaviour {
     public static OptionsPanel Instance { get; private set; }
 
     [SerializeField] private GameObject container;
 
+    [Header("Tabs")]
+    [SerializeField] private GameObject controlsTab;
+    [SerializeField] private GameObject soundTab;
+    [SerializeField] private GameObject languageTab;
+
+
     [Header("Sliders")]
     [SerializeField] private Slider masterVolumeSlider;
     [SerializeField] private Slider mouseSensitivitySlider;
 
+
     [Header("Buttons UI")]
+    [SerializeField] private TextMeshProUGUI controlTabBtnText;
+    [SerializeField] private TextMeshProUGUI soundTabBtnText;
+    [SerializeField] private TextMeshProUGUI languageTabBtnText;
     [SerializeField] private TextMeshProUGUI backBtnText;
     [SerializeField] private TextMeshProUGUI arrowLeftBtnText;
     [SerializeField] private TextMeshProUGUI arrowRightBtnText;
@@ -47,11 +63,43 @@ public class OptionsPanel : MonoBehaviour {
         IsActive = true;
     }
     public void Hide() {
+        controlTabBtnText.color = Color.white;
+        soundTabBtnText.color = Color.white;
+        languageTabBtnText.color = Color.white;
+
         arrowLeftBtnText.color = Color.white;
         arrowRightBtnText.color = Color.white;
         backBtnText.color = Color.white;
         container.SetActive(false);
         IsActive = false;
+    }
+
+    public void OpenControlTab() {
+        if (controlsTab.activeInHierarchy) {
+            return;
+        }
+        controlsTab.SetActive(true);
+
+        soundTab.SetActive(false);
+        languageTab.SetActive(false);
+    }
+    public void OpenSoundTab() {
+        if (soundTab.activeInHierarchy) {
+            return;
+        }
+        soundTab.SetActive(true);
+
+        controlsTab.SetActive(false);
+        languageTab.SetActive(false);
+    }
+    public void OpenLanguageTab() {
+        if (languageTab.activeInHierarchy) {
+            return;
+        }
+        languageTab.SetActive(true);
+
+        controlsTab.SetActive(false);
+        soundTab.SetActive(false);
     }
 
     private void OnChangeMasterVolume(float val) {
